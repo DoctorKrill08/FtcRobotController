2fb1beeabefc760f8f1e704450a1fba6c6002ab3
DoctorKrill08 <doctorkrill08@gmail.com>
2026-06-19 22:08:26 -0400

    Code Camp

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Intake.java
@@ -0,0 +1,44 @@
+package org.firstinspires.ftc.teamcode.Subsystems;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Intake {
+    DcMotor intake;
+    DcMotor transfer;
+
+    public enum State{
+        RESTING,
+        INTAKING,
+        EJECTING
+    }
+    State currentState = State.RESTING;
+
+    public State getState(){
+        return currentState;
+    }
+    public void setState(State newState){
+        currentState = newState;
+    }
+    public void initiate(HardwareMap hardwareMap){
+        intake = hardwareMap.dcMotor.get("intake");
+        transfer = hardwareMap.dcMotor.get("transfer");
+    }
+    public void update(){
+        switch (currentState){
+            case RESTING:
+                intake.setPower(0);
+                transfer.setPower(0);
+                break;
+            case INTAKING:
+                intake.setPower(1);
+                transfer.setPower(1);
+                break;
+            case EJECTING:
+                intake.setPower(-1);
+                transfer.setPower(-1);
+                break;
+        }
+    }
+
+}
