a4a8913b9627fd32a8e1770ba3180666339b0107
DoctorKrill08 <doctorkrill08@gmail.com>
2026-06-24 18:42:52 -0400

    kjbgsegsdinokjermn mn

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Flywheel.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Flywheel.java
@@ -1,11 +1,12 @@
 package org.firstinspires.ftc.teamcode.Subsystems;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
-
+@Config
 public class Flywheel {
     public enum State{
         ON,
@@ -21,7 +22,8 @@ public class Flywheel {
     DcMotorEx rightMotor;
     DcMotorEx leftMotor;
     public static int target = 1250;
-    public static double kF = 0.00025;
+    public static double kF = 0.00041;
+    public static double kP = 0.021;
     public void initiate(HardwareMap hardwareMap){
         rightMotor = hardwareMap.get(DcMotorEx.class,"flyRight");
         leftMotor = hardwareMap.get(DcMotorEx.class,"flyLeft");
@@ -31,6 +33,8 @@ public class Flywheel {
         double targetPower = 0;
         if (state == State.ON){
             targetPower = target * kF;
+            double error = target - rightMotor.getVelocity();
+            targetPower += (error * kP);
         }
         rightMotor.setPower(targetPower);
         leftMotor.setPower(targetPower);
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Gate.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Gate.java
@@ -1,16 +1,5 @@
 package org.firstinspires.ftc.teamcode.Subsystems;
 
 public class Gate {
-    public static long OPEN_TIME = 1000; //How many milis the gate is open for.
-    Stopwatch gateTimer = new Stopwatch();
-    public void shoot(){
-        gateTimer.go();
-        //Set the state to open
-    }
-    public void update(){
-        //Times passed is more than 1000 milis
-        if (gateTimer.timePassed() > OPEN_TIME){
-            //Do something
-        }
-    }
+
 }
